e29c26646cd6978acd5690ae8e578d9eb91f2e9e
Viviane Araujo <a.viviane@gmail.com>
2022-12-31 20:17:09 -0300

    Criação da classe Conta e dos métodos principais

--- app.py
+++ app.py
@@ -8,3 +8,23 @@ Faça um programa de caixa eletrônico com as opções:
 O saldo inicial é sempre de R$ 0,00. 
 A cada saque ou depósito atualize o valor do saldo e apresente-o na tela.
 '''
+
+class Conta():
+    def __init__(self, nome,saldo = 0 ):        
+        self.saldo = saldo
+        self.nome = nome
+
+    def consulta_saldo(self):
+        print(f'Olá {self.nome} seu saldo é: {self.saldo:.2f}') 
+
+    def saque(self,valor):
+        if self.saldo >= valor:
+            self.saldo -= valor
+        else:
+            print('Saldo insuficiente!')
+
+    def deposito(self,valor):
+        self.saldo += valor
+
+
+
