e70ec7ee4ddd5e414708b828e6289299409d986f
Viviane Araujo <a.viviane@gmail.com>
2022-12-31 14:08:10 -0300

    Criando o arquivo app

---
+++ app.py
@@ -0,0 +1,10 @@
+'''
+ATM project
+Faça um programa de caixa eletrônico com as opções: 
+(1) consulta saldo, 
+(2) saque, 
+(3) depósito e 
+(4) sair. 
+O saldo inicial é sempre de R$ 0,00. 
+A cada saque ou depósito atualize o valor do saldo e apresente-o na tela.
+'''
